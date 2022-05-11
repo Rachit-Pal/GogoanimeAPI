cfd37e13a39885ef8541b6c5d938974a7470ff1f
futaba <mythhb@yahoo.com>
2022-05-11 13:16:16 -0400

    feat: renamed `Anime` type to `Gogoanime`

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -6,13 +6,13 @@ export type AnimeList = {
   status?: string;
 };
 
-export type Episode = {
+export type GogoEpisode = {
   episodeId?: string;
   episodeNum?: number | string;
   episodeUrl?: string;
 };
 
-export type Anime = {
+export type Gogoanime = {
   animeId?: string;
   type?: string;
   animeTitle?: string;
@@ -22,22 +22,22 @@ export type Anime = {
   otherNames?: string[] | string;
   synopsis?: string;
   totalEpisodes?: number | string;
-  episodesList?: Episode[];
+  episodesList?: GogoEpisode[];
 };
 
 /**
  * @typedef {Object} AnimeList
- * @param {Anime[]} list out put of the anime list
+ * @param {AnimeList[]} list out put of the anime list
  * @param {string} keyw query keyword to search
  * @param {string} page current page
  */
-export declare function scrapeSearch(list: ?AnimeList[], keyw: string, page: number = 1): Promise<AnimeList[]>;
+export declare function scrapeSearch(list: ?AnimeList[], keyw: string, page: number = 1): Promise<?AnimeList[]>;
 
 /**
  *
  * @param animeId anime id to scrape the anime info
- * @returns {Anime} anime info object
+ * @returns {Gogoanime} anime info object
  */
-export declare function scrapeAnimeDetails(animeId: string): Promise<Anime>;
+export declare function scrapeAnimeDetails(animeId: string): Promise<?Gogoanime>;
 
 // TODO: add more functions
